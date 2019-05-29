a3669aed2d8c3f3548ad66cc74810bdf2d53d328
jean72027 <50391443+jean72027@users.noreply.github.com>
2019-05-29 11:43:20 +0800

    Add files via upload

--- sketch_may29a_0529.ino
+++ sketch_may29a_0529.ino
@@ -1,18 +1,30 @@
-
+const int buttonPin = 2;
+int buttonState = 0;
 int latchPin = 8;
 int clockPin = 12;
 int dataPin = 11;
+
+//B01111111,B10111111
 byte colDataMatrix[8] = { B01111111,
-   B10111111,
-   B11011111,
-   B11101111,
-   B11110111,
-   B11111011,
-   B11111101,
-   B11111110
-  };
-                      
-byte rowDataMatrix[8] = { B00011000,
+
+B10111111,
+
+B11011111,
+
+B11101111,
+
+B11110111,
+
+B11111011,
+
+B11111101,
+
+B11111110
+
+};
+
+//B11111111,B10000001
+byte rowDataMatrix[8] = {  B00011000,
             B00111100,
     B00011000,
     B10011001,
@@ -20,33 +32,143 @@ byte rowDataMatrix[8] = { B00011000,
     B11100111,
     B10111101,
     B11111111
-  };
- // 定義顯示器顯示的時間 (延遲時間), 預設 1ms
+
+};
+
+// 定義顯示器顯示的時間 (延遲時間), 預設 1ms
+
 int delay_time = 1;
-      
+
 void setup() {
+
 pinMode(latchPin,OUTPUT);
+
 pinMode(clockPin,OUTPUT);
+
 pinMode(dataPin,OUTPUT);
+
+pinMode(buttonPin,INPUT);
+  
+digitalWrite(buttonPin,HIGH);
 }
+
 void loop() {
- for(int i = 0; i < 8 ; i++){
- //byte colData = colDataMatrix[i];
+buttonState = digitalRead(buttonPin);
+if(buttonState == LOW){
+ 
+for(int i = 0; i < 8 ; i++){
+
+//byte colData = colDataMatrix[i];
+
 digitalWrite(latchPin,LOW);
-  //  pickDigit(i);
-   shiftOut(dataPin, clockPin, MSBFIRST, colDataMatrix[i]);
-    shiftOut(dataPin, clockPin, MSBFIRST, rowDataMatrix[i]);
-    //shiftOut(dataPin, clockPin, MSBFIRST, colDataMatrix[i]);
-    digitalWrite(latchPin,HIGH);
-    delay(delay_time);
+
+// pickDigit(i);
+
+shiftOut(dataPin, clockPin, MSBFIRST, colDataMatrix[i]);
+
+shiftOut(dataPin, clockPin, MSBFIRST, rowDataMatrix[i]);
+
+//shiftOut(dataPin, clockPin, MSBFIRST, colDataMatrix[i]);
+
+digitalWrite(latchPin,HIGH);
+
+delay(delay_time);
+
   }
 }
-void pickDigit(int x) {
-  byte rowByte = 1 << x;
-  shiftOut(dataPin, clockPin, MSBFIRST, rowByte);
+else if(buttonState == HIGH){
+digitalWrite(latchPin, 0);
+  shiftOut(dataPin, clockPin, B01111111);  //mask(col)
+  shiftOut(dataPin, clockPin, B00111100);  //row
+  digitalWrite(latchPin, 1);
+
+  digitalWrite(latchPin, 0);
+  shiftOut(dataPin, clockPin, B10111111);  //mask(col)
+  shiftOut(dataPin, clockPin, B01000010);  //row
+  digitalWrite(latchPin, 1);
+
+  digitalWrite(latchPin, 0);
+  shiftOut(dataPin, clockPin, B11011111);  //mask(col)
+  shiftOut(dataPin, clockPin, B10100101);  //row
+  digitalWrite(latchPin, 1);
+
+  digitalWrite(latchPin, 0);
+  shiftOut(dataPin, clockPin, B11101111);  //mask(col)
+  shiftOut(dataPin, clockPin, B10000001);  //row
+  digitalWrite(latchPin, 1);
+
+  digitalWrite(latchPin, 0);
+  shiftOut(dataPin, clockPin, B11110111);  //mask(col)
+  shiftOut(dataPin, clockPin, B10100101);  //row
+  digitalWrite(latchPin, 1);
+
+  digitalWrite(latchPin, 0);
+  shiftOut(dataPin, clockPin, B11111011);  //mask(col)
+  shiftOut(dataPin, clockPin, B10011001);  //row
+  digitalWrite(latchPin, 1);
+
+  digitalWrite(latchPin, 0);
+  shiftOut(dataPin, clockPin, B11111101);  //mask(col)
+  shiftOut(dataPin, clockPin, B01000010);  //row
+  digitalWrite(latchPin, 1);
+
+  digitalWrite(latchPin, 0);
+  shiftOut(dataPin, clockPin, B11111110);  //mask(col)
+  shiftOut(dataPin, clockPin, B00111100);  //row
+  digitalWrite(latchPin, 1);
+}
 
 }
 
+void pickDigit(int x) {
 
- 
+byte rowByte = 1 << x;
 
+shiftOut(dataPin, clockPin, MSBFIRST, rowByte);
+
+}
+void shiftOut(int myDataPin, int myClockPin, byte myDataOut) {
+  // This shifts 8 bits out MSB first, 
+  //on the rising edge of the clock,
+  //clock idles low
+
+  //internal function setup
+  int i=0;
+  int pinState;
+  pinMode(myClockPin, OUTPUT);
+  pinMode(myDataPin, OUTPUT);
+
+  //clear everything out just in case to
+  //prepare shift register for bit shifting
+  digitalWrite(myDataPin, 0);
+  digitalWrite(myClockPin, 0);
+
+  //for each bit in the byte myDataOut&#239;&#191;&#189;
+  //NOTICE THAT WE ARE COUNTING DOWN in our for loop
+  //This means that %00000001 or "1" will go through such
+  //that it will be pin Q0 that lights. 
+  for (i=7; i>=0; i--)  {
+    digitalWrite(myClockPin, 0);
+
+    //if the value passed to myDataOut and a bitmask result 
+    // true then... so if we are at i=6 and our value is
+    // %11010100 it would the code compares it to %01000000 
+    // and proceeds to set pinState to 1.
+    if ( myDataOut & (1<<i) ) {
+      pinState= 1;
+    }
+    else {        
+      pinState= 0;
+    }
+
+    //Sets the pin to HIGH or LOW depending on pinState
+    digitalWrite(myDataPin, pinState);
+    //register shifts bits on upstroke of clock pin  
+    digitalWrite(myClockPin, 1);
+    //zero the data pin after shift to prevent bleed through
+    digitalWrite(myDataPin, 0);
+  }
+
+  //stop shifting
+  digitalWrite(myClockPin, 0); 
+}
